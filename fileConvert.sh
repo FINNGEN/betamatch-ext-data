e6b81eeb1442f5ff5ec7bd0f9264e6c61ae162b6
Kumar Veerapen <mkveerapen@gmail.com>
2021-09-29 14:09:16 -0400

    adding prostate cancer

--- fileConvert.sh
+++ fileConvert.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-output=`ls *.txt`
-
 cd data/
-mkdir editedFiles
+output=`ls *.txt *.csv`
 
-for i in $(ls *.txt *.csv)
+for i in $output
 do
-	file $i
-	cat $i | tr -d '\r' | iconv -c -f UTF-8 -t LATIN1 > $i-edited
-	file $i-edited 
-	mv $i-edited editedFiles
-	D=`echo $i-edited | awk -F- '{print $1}'`
-	cd editedFiles
-	mv $i-edited $D
-	cd ../
+	isCRLF=$(file $i)
+	echo $isCRLF
+
+	if [[ $isCRLF =~ ", with CRLF line terminators" ]]
+	then
+		echo "processing $i"
+		file $i
+		cat $i | tr -d '\r' | iconv -c -f UTF-8 -t LATIN1 > $i-edited
+		file $i-edited 
+		D=`echo $i-edited | awk -F- '{print $1}'`
+		mv $i-edited $D
+	else 
+		echo "not it"
+	fi
 done
